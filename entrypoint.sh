dd87501a79575629652f8097ed1113e717507917
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2021-09-08 15:29:43 +0200

    improve(entrypoint): use reference for base branch
    - ensures that the action will continue to work correctly
      when we create the 7.16 maintenance branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ git fetch
 apt update && apt -y install ruby
 gem install octokit
 
-if [[ $(git diff origin/master HEAD --name-only | grep pom.xml$ | wc -c) -ne 0 ]]; then
+if [[ $(git diff origin/$GITHUB_BASE_REF HEAD --name-only | grep pom.xml$ | wc -c) -ne 0 ]]; then
     apt install -y nodejs npm rsync
 
     cd /github/workspace
@@ -16,14 +16,14 @@ if [[ $(git diff origin/master HEAD --name-only | grep pom.xml$ | wc -c) -ne 0 ]
     mvn -T 16C -s /maven-settings.xml clean package -DskipTests -Dskip-third-party-bom=false -Dthird-party-bom-scopes="compile|provided|runtime|test"
     find . -name 'dependencies.txt' -exec rsync -R \{\} /pr \;
 
-    git checkout -f origin/master
+    git checkout -f origin/GITHUB_BASE_REF
     mvn -T 16C -s /maven-settings.xml clean package -DskipTests -Dskip-third-party-bom=false -Dthird-party-bom-scopes="compile|provided|runtime|test"
-    find . -name 'dependencies.txt' -exec rsync -R \{\} /master \;
+    find . -name 'dependencies.txt' -exec rsync -R \{\} /base \;
 
     echo -e "<details><summary>Dependency Tree Diff</summary><p>\n" >/github/workspace/dep-tree-diff.txt
     echo "\`\`\`diff" >>/github/workspace/dep-tree-diff.txt
 
-    diff -r /master /pr >>/github/workspace/dep-tree-diff.txt || :
+    diff -r /base /pr >>/github/workspace/dep-tree-diff.txt || :
     # || : => : is the null command that always succeeds; 
     # this way we can ignore the exit code of which is 1 in case the files differ
 
