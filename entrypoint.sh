1c3acfd94ab5d4d529a5b5722cb3f53834e405e4
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2021-09-08 11:06:08 +0200

    improve(entrypoint): make script stop if a Maven command fails
    - replaces git pull with fetch (since there is no checked out branch)
    - ignores exit code of diff because it returns 1 in case the files
      differ, which is an expected case

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-git pull
+set -e
+# let script stop when one of the commands fails (e.g. one of the Maven builds)
+
+git fetch
 
 apt update && apt -y install ruby
 gem install octokit
@@ -19,7 +22,11 @@ if [[ $(git diff origin/master HEAD --name-only | grep pom.xml$ | wc -c) -ne 0 ]
 
     echo -e "<details><summary>Dependency Tree Diff</summary><p>\n" >/github/workspace/dep-tree-diff.txt
     echo "\`\`\`diff" >>/github/workspace/dep-tree-diff.txt
-    diff -r /master /pr >>/github/workspace/dep-tree-diff.txt
+
+    diff -r /master /pr >>/github/workspace/dep-tree-diff.txt || :
+    # || : => : is the null command that always succeeds; 
+    # this way we can ignore the exit code of which is 1 in case the files differ
+
     echo "\`\`\`" >>/github/workspace/dep-tree-diff.txt
     echo "</p></details>" >>/github/workspace/dep-tree-diff.txt
 
