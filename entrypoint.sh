e598b9b33fe4ddf81fb549fc8c8ce95229e3be4a
tasso94 <3015690+tasso94@users.noreply.github.com>
2022-11-22 11:11:12 +0100

    chore(nodejs): pin version of NodeJS to 14.X

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,10 @@ apt update && apt -y install ruby bundler
 bundle install --gemfile=/Gemfile
 
 if [[ $(git diff origin/$GITHUB_BASE_REF HEAD --name-only | grep pom.xml$ | wc -c) -ne 0 ]]; then
+    # Pin version of NodeJS to 14.X
+    curl -fsSL https://deb.nodesource.com/setup_14.x | bash -
+    apt update
+    
     apt install -y nodejs npm rsync
 
     cd /github/workspace
